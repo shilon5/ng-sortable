78ab76a5faea89687acde3b801ba2168cca6cc98
Niro <nir.shilon@hp.com>
2015-03-21 23:36:11 +0200

    support vs

---
+++ demo/scripts/directives/flexHeight.js
@@ -0,0 +1,40 @@
+/*jshint undef: false, unused: false, indent: 2*/
+/*global angular: false */
+
+'use strict';
+
+angular.module('demoApp').directive('flexHeight', ['$window', '$timeout', '$log', function ($window, $timeout, $log) {
+
+    function getOffsetTop( el ){
+        var ot = 0;
+
+        while ( !!el ){
+            ot += el.offsetTop;
+            el = el.offsetParent;
+        }
+
+        return ot;
+    }
+
+    return {
+        scope: false,
+        restrict: 'A',
+        link: {
+            post: function (scope, element) {
+                scope.$watch(function(){
+                    return getOffsetTop(element[0]);
+                }, function(newVal, oldVal){
+
+                    var top = newVal,
+                        h = $window.innerHeight - top;
+
+                    $log.debug($window.innerHeight + ' - ' + top, h);
+
+                    element.css('max-height', h + 'px');
+                })
+
+            }
+        }
+    };
+
+}]);
